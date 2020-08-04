3540312112debedbb1fbc0b569251da06c9d32a6
SutrisDevPemula <sutris.devpemula@gmail.com>
2020-08-04 11:43:09 +0800

    update galerry

--- resources/views/home/gallery.blade.php
+++ resources/views/home/gallery.blade.php
@@ -4,16 +4,16 @@ active
 @endsection
 @section('header')
 {{-- slider --}}
-<img src="{{ asset('images/hutan.png') }}" class="img-fluid" alt="Responsive image">
-<p>&nbsp;</p>
+<img src="{{ asset('images/hutan.png') }}" class="img-fluid mb-5" alt="Responsive image">
 <h5 class="judul-section " style="font-family:OpenSans-Bold;font-size:25px; font-weight: normal;">
-    <strong>Galerry</strong></h5><br>
-<div class="garis"></div><br>
+    <strong>Galerry</strong></h5>
+<div class="garis mb-3"></div><br>
 <div class="container mb-5">
     <div class="row">
         @foreach ($photos as $photo)
         <div class="col-lg-3" style="margin-bottom: 30px">
             <img src="{{ asset('storage/'.$photo->photo) }}" class="card-img" width="100px" height="200px">
+        <p class="text-bodyb mt-1 bg-green p-3" style="color: white">{{$photo->description}}</p>
         </div>
         @endforeach
 
