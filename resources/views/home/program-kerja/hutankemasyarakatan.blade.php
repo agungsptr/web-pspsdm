22f0f146ad7232b6e46dd25a65b9ecd9d4f61d20
SutrisDevPemula <56956940+SutrisDevPemula@users.noreply.github.com>
2021-01-22 01:58:33 -0800

    english content done

--- resources/views/home/program-kerja/hutankemasyarakatan.blade.php
+++ resources/views/home/program-kerja/hutankemasyarakatan.blade.php
@@ -11,12 +11,11 @@
         <div class="carousel-item active">
             <div class="card-deck">
                 <div class="card">
-                    <img class="d-block" src="{{ asset('images/hutan/1.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                    <img class="d-block" src="{{ asset('images/hutan/1.jpg') }}" alt="Card image cap" height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/2.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/3.jpg') }}" alt="Card image cap"
@@ -36,24 +35,23 @@
         <div class="carousel-item">
             <div class="card-deck">
                 <div class="card">
-                    <img class="d-block" src="{{ asset('images/hutan/6.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                    <img class="d-block" src="{{ asset('images/hutan/6.jpg') }}" alt="Card image cap" height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/7.png') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/8.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/9.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/10.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
             </div>
         </div>
@@ -61,24 +59,23 @@
         <div class="carousel-item">
             <div class="card-deck">
                 <div class="card">
-                    <img class="d-block" src="{{ asset('images/hutan/11.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                    <img class="d-block" src="{{ asset('images/hutan/11.jpg') }}" alt="Card image cap" height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/12.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/1.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/2.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
                 <div class="card">
                     <img class="card-img-top" src="{{ asset('images/hutan/3.jpg') }}" alt="Card image cap"
-                    height="290cm">
+                        height="290cm">
                 </div>
             </div>
         </div>
@@ -96,10 +93,21 @@
 {{-- content --}}
 <div class="container" style="font-family: 'Montserrat', sans-serif;">
     <div class="p text-justify mb-5" style="font-weight: bold">
-        <p>Program HKm bertujuan meningkatkan kesejahteraan masyarakat setempat melalui pemanfaatan sumberdaya hutan secara optimal, adil dan berkelanjutan dengan tetap menjaga kelestarian fungsi hutan dan lingkungan hidup.  PSPSDM memiliki pengalaman kegiatan HKm dalam bentuk Penyuluhan dan bimbingan teknis pembangunan hutan kemasyarakatan seluas 1.500 hektar untuk masyarakat desa hutan di wilayah HKm Sekotong, Sekaroh dan Sambelia di Pulau Lombok.   Bekerjasama dengan Balai Rehabilitasi Lahan dan Konservasi Tanah Propinsi Nusa Tenggara barat dengan bantuan dana hibah dari Overseas Econimic Cooperation Fund (OECF) Jepang.</p>
+        <p>Program HKm bertujuan meningkatkan kesejahteraan masyarakat setempat melalui pemanfaatan sumberdaya hutan
+            secara optimal, adil dan berkelanjutan dengan tetap menjaga kelestarian fungsi hutan dan lingkungan hidup.
+            PSPSDM memiliki pengalaman kegiatan HKm dalam bentuk Penyuluhan dan bimbingan teknis pembangunan hutan
+            kemasyarakatan seluas 1.500 hektar untuk masyarakat desa hutan di wilayah HKm Sekotong, Sekaroh dan Sambelia
+            di Pulau Lombok. Bekerjasama dengan Balai Rehabilitasi Lahan dan Konservasi Tanah Propinsi Nusa Tenggara
+            barat dengan bantuan dana hibah dari Overseas Econimic Cooperation Fund (OECF) Jepang.</p>
     </div>
     <div class="p text-justify mb-5" style="font-style: italic">
-        <p>Social Forestry (HKm) program aims to improve local community welfare through optimal, fair, and sustainable forest resources utilization by maintaining the preservation of forest functions and the environment. PSPSDM has experiences in Social Forestry  program in the program of extension and technical guidance on social forestry development involving 1,500 ha area for forest village communites in HKm areas of Sekotong, Sekaroh, and Sambelia in Lombok Island. The program is in cooperation with the Center for Land Rehabilitation and Soil Conservation of West Nusa Tenggara Province with grant from Overseas Economic Cooperation Fund (OECF) Japan.</p>
+        <p>Social Forestry (HKm) program aims to improve local community welfare through optimal, fair, and sustainable
+            forest resources utilization by maintaining the preservation of forest functions and the environment. PSPSDM
+            has experiences in Social Forestry program in the program of extension and technical guidance on social
+            forestry development involving 1,500 ha area for forest village communites in HKm areas of Sekotong,
+            Sekaroh, and Sambelia in Lombok Island. The program is in cooperation with the Center for Land
+            Rehabilitation and Soil Conservation of West Nusa Tenggara Province with grant from Overseas Economic
+            Cooperation Fund (OECF) Japan.</p>
     </div>
 </div>
 @endsection
