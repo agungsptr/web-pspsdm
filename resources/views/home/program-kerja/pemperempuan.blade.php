ba4e9e76c76698ae670ab3042d9d93710db33fee
agungsptr <agung.eka1999@gmail.com>
2021-01-22 10:12:22 +0000

    pemeberdayaan perempuan done

--- resources/views/home/program-kerja/pemperempuan.blade.php
+++ resources/views/home/program-kerja/pemperempuan.blade.php
@@ -4,109 +4,110 @@
 <img src="{{ asset('images/pemberdayaanperempuan\slider/1.jpg') }}" class="d-block w-100" alt="...">
 
 {{-- content --}}
-<h1 class="mb-3 text-center" style="font-weight: bold">Program Pemberdayaan Perempuan</h1>
+<h1 class="mb-3 mt-5 text-center" style="font-weight: bold">Program Pemberdayaan Perempuan</h1>
+<h2 class="mb-3 text-center" style="font-weight: bold; font-style: italic">Women Empowerment Program </h2>
 <div class="garis mb-5"></div>
-<div id="carouselExampleControls" class="carousel slide bg-green mb-3 p-5" data-ride="carousel">
+
+<div id="carouselExampleControls2" class="carousel slide bg-green mb-3 p-5" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
             <div class="card-deck">
                 <div class="card">
-                    <img class="card-img-top" src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                        alt="Card image cap">
+                    <img class="d-block" src="{{ asset('images/pemberdayaanperempuan/1.jpg') }}" alt="Card image cap"
+                    height="290cm">
                 </div>
                 <div class="card">
-                    <img class="card-img-top" src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                        alt="Card image cap">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/2.jpg') }}" alt="Card image cap"
+                    height="290cm">
                 </div>
                 <div class="card">
-                    <img class="card-img-top" src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                        alt="Card image cap">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/3.jpg') }}" alt="Card image cap"
+                        height="290cm">
                 </div>
                 <div class="card">
-                    <img class="card-img-top" src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                        alt="Card image cap">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/4.jpg') }}" alt="Card image cap"
+                        height="290cm">
                 </div>
                 <div class="card">
-                    <img class="card-img-top" src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                        alt="Card image cap">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/5.jpg') }}" alt="Card image cap"
+                        height="290cm">
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class="card-deck">
+                <div class="card">
+                    <img class="d-block" src="{{ asset('images/pemberdayaanperempuan/6.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/7.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/8.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/9.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/10.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <div class="card-deck">
+                <div class="card">
+                    <img class="d-block" src="{{ asset('images/pemberdayaanperempuan/11.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/1.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/2.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/3.jpg') }}" alt="Card image cap"
+                    height="290cm">
+                </div>
+                <div class="card">
+                    <img class="card-img-top" src="{{ asset('images/pemberdayaanperempuan/4.jpg') }}" alt="Card image cap"
+                    height="290cm">
                 </div>
             </div>
         </div>
     </div>
     {{-- previus and next --}}
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <a class="carousel-control-prev" href="#carouselExampleControls2" role="button" data-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
         <span class="sr-only">Previous</span>
     </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <a class="carousel-control-next" href="#carouselExampleControls2" role="button" data-slide="next">
         <span class="carousel-control-next-icon" aria-hidden="true"></span>
         <span class="sr-only">Next</span>
     </a>
 </div>
+
 {{-- content --}}
 <div class="container" style="font-family: 'Montserrat', sans-serif;">
-    <div class="p text-justify mb-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis, nostrum.
-        Possimus
-        quod quae,
-        earum et animi nulla, pariatur repellendus debitis error saepe non numquam quidem repudiandae modi quaerat,
-        deleniti quisquam?<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem pariatur iusto
-            dolorem iste, ex reprehenderit officia quod recusandae. Ullam dolorem quasi nulla necessitatibus velit odit
-            deleniti vero eius enim ipsa?<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolore
-                placeat mollitia excepturi voluptatum, est officiis quos veritatis ullam voluptates, quibusdam ipsa
-                consequuntur eveniet ea sunt veniam reiciendis labore totam?</p>
-            deleniti vero eius enim ipsa?<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolore
-                placeat mollitia excepturi voluptatum, est officiis quos veritatis ullam voluptates, quibusdam ipsa
-                consequuntur eveniet ea sunt veniam reiciendis labore totam?</p>
-        </p>
+    <div class="p text-justify mb-5" style="font-weight: bold">
+        <p>Pemberdayaan Perempuan bertujuan untuk membangun kesadaran perempuan tentang kesetaraan gender agar mampu mengembangkan potensi yang ada pada dirinya, sehingga perempuan dapat mandiri dan berpartisipasi dalam pembangunan.</p>
+        <p>Program pemberdayaan perempuan yang telah dilaksanakan PSPSDM adalah pemberdayaan kelompok perempuan tani pada desa hutan Gunung Rinjani, termasuk untuk kelompok perempuan masyarakat adat.  Mereka mendapat layanan pendampingan dan pelatihan pengelolaan hasil paska panen seperti pengolahan minyak kelapa murni (Virgin Coconut Oil / VCO), pengolahan kopi dan serbat jahe, serta pengolahan hasil panen lainnya (keripik pisang, nangka, jagung). Dampingan yang diberikan berupa penguatan kapasitas kelembagaan kelompok, peningkatan kualitas produk, pengemasan/packaging dan pengembangan kerjasama jaringan pemasaran.</p>
+        <p>Donor progam yang telah memberikan dukungan program Pemberdayaan Perempuan adalah  Green Prosperity Project Facility - MCA Indonesia.</p>
     </div>
 
-    <div class="card mb-3 shadow">
-        <h6 class="mb-3 mt-3 ml-2 card-title" style="font-weight: bold">Program Pemberdayaan Perempuan</h6>
-        <p class="card-text ml-3">This is a longer card with supporting text below as a natural lead-in to additional
-            content. This content is a little bit longer.</p>
-        <div id="carouselExampleControls" class="card-body carousel slide bg-transparent" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="card-deck">
-                        <div class="card">
-                            <img class="card-img-top"
-                                src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                                alt="Card image cap">
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top"
-                                src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                                alt="Card image cap">
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top"
-                                src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                                alt="Card image cap">
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top"
-                                src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                                alt="Card image cap">
-                        </div>
-                        <div class="card">
-                            <img class="card-img-top"
-                                src="https://via.placeholder.com/125/000000/FFFFFF/?text=IPaddress.net"
-                                alt="Card image cap">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {{-- previus and next --}}
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
+    <div class="p text-justify mb-5" style="font-style: italic">
+        <p>Women empowerment aims to establish women awareness on gender equality to be able to develop their own potential so they could be independent and participate in the development.</p>
+        <p>The women empowerment programs that have been conducted by PSPSDM are women farmer group empowerment in forest villages of Rinjani Mountain including women groups of indigenous people. They receive assistance and training services of post-harvest yield management, such as Virgin Coconut Oil (VCO) processing, coffee and ginger processing and other harvest yield processing (banana, jackfruit, and corn chips), product quality improvement, packaging, and cooperation development of marketing network.</p>
+        <p>The program donator that has supported the mother and child health program of PSPSDM  is Green Prosperity Project Facility - MCA Indonesia.</p>
     </div>
-
 </div>
 @endsection
